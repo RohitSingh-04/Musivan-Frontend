e8dab4a117cd16cb639ff673b1fe2e41bc0000b2
Rohit Singh <rsingh.py@gmail.com>
2025-08-18 21:12:25 +0530

    login page redesign

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,12 +21,13 @@ const Login = () => {
   }
 
   return (
-    <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
-      <div className='flex flex-col items-center bg-slate-700 rounded p-10 h-[40vh]'>
-        <h1 className='text-white'>Login</h1>
-        <input type="text" placeholder='username' className='border-green-700 border m-2 p-2 rounded-2xl text-white' onChange={(e)=>{setUsername(e.target.value)}}/>
-        <input type="password" placeholder='password' id="passwd" className='border-green-700 border m-2 p-2 rounded-2xl text-white' onChange={(e)=>{setPassword(e.target.value)}}/>
-        <button className='bg-[#00FF00] text-black text-[15px] px-4 py-1 rounded-2xl cursor-pointer w-[10vh]' onClick={login}>Login</button>
+    <div className='w-[100vw] h-[100vh] justify-center bg-black'>
+      <div className='flex flex-col items-center bg-black rounded p-10'>
+        <h1 className='text-white text-[50px] mb-9'>Hii Welcome Again! Login</h1>
+        
+        <input type="text" placeholder='username' className='border m-2 p-2 rounded-xl text-white w-[290px]' onChange={(e)=>{setUsername(e.target.value)}}/>
+        <input type="password" placeholder='password' id="passwd" className='border mb-5 p-2 rounded-xl text-white w-[290px]' onChange={(e)=>{setPassword(e.target.value)}}/>
+        <button className='bg-[#00FF00] text-black text-[15px] px-4 py-2 rounded-2xl cursor-pointer w-[290px] mb-10' onClick={login}>Login</button>
         <div><span className='text-white'>Don't have an account? <button className='cursor-pointer underline' onClick={()=>{navigate("/register")}}>Register Here</button></span></div>
       </div>
     </div>
